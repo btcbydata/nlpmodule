739a6a0abd4b64bb063d3cbce7b1d638cd71a541
hoonsw23 <porkpitch@naver.com>
2022-10-19 22:07:16 +0900

    feature:detect words from csv data csv 데이터에서 코인 관련 글만 따로 뽑아 csv 파일로 만드는 스크립터 추가. 주석은 단어와 코인 매칭.

--- detectraw.py
+++ detectraw.py
@@ -1,12 +1,15 @@
 import pandas as pd
 
-id = []
-document = []
-label = []
-filename='testdata'
+data = pd.read_csv("btcbd_nlp\\bitcoingallery_csv\\202001.csv")
 
-data=pd.read_csv("btcdbd_nlp\\bitcoingallery_csv\\202001.csv")
+data = data.drop(['date'],axis=1)
+df = data[data['title'].str.contains('비트')]
+df.to_csv("bit.csv", encoding='utf-8-sig')
 
-data.query('title.str.contains("비트")')
 
-data
+"""
+
+word match
+BITCOIN = "비트" FLOW = "플로우" NEM = "넴" TOPSHOT = "탑샷" CARDANO = "카르" ANKER = "앵커" ARGO = "아르고" METAL = "메탈" EDchain = "에드" ORBS = "오브스" CSPR = "캐스퍼" MARO = "마로" LUNA = "루나" PUNDIX = "펀디" MVL = "엠블" ENJIN = "엔진" MBL = "무비" PXL = "픽셀" MANA = "마나" TRON = "트론" HBAR = "헤데" CHR = "크로미아"
+
+"""
